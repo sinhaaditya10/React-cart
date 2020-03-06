52cc783f3d0dac0ded61ca97e3b3ca27d666354e
Aditya Sinha <aditya.sinha@comviva.com>
2020-03-06 15:49:01 +0530

    cart left

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,32 @@ import "./component.css";
 import CartLogo from "./cart.svg";
 
 export class NavBar extends Component {
+  showCart() {}
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-          <span className="home-logo">
+          <div className="home-logo">
             React
             <img
-              className="nav-logo"
               src={CartLogo}
               alt=""
-              style={{ width: "40px", height: "40px", marginTop: "5px" }}
+              style={{
+                width: "40px",
+                height: "40px",
+                marginTop: "5px",
+                marginBottom: "0px"
+              }}
             />
-          </span>
-          <span className="home-welcome">Welcome {this.props.user}</span>
+          </div>
+          <div>
+            <a href="#" style={{ marginLeft: "900px", color: "white" }}>
+              Cart
+            </a>
+          </div>
+          <div className="home-welcome">
+            <span>Welcome {this.props.user}</span>
+          </div>
         </nav>
       </div>
     );
